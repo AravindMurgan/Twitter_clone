425100925ec7366fb23e85b45aba9955925c8ac0
Aravind Murugan <mi.arvindmurugan@gmail.com>
2021-07-13 15:32:40 +0530

    integrate-firebase

---
+++ src/component/Firebase/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyD3cnAmGR0Q4Mu6KXgqqt0AggI3uWZSscw",
+    authDomain: "twitter-clone-f5ddb.firebaseapp.com",
+    projectId: "twitter-clone-f5ddb",
+    storageBucket: "twitter-clone-f5ddb.appspot.com",
+    messagingSenderId: "206482336826",
+    appId: "1:206482336826:web:6ef6cdbac03225975a9903"
+  };
